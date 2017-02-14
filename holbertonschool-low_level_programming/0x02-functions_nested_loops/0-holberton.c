3ffeedd2d6f0f743a1da2e69593c57b99292e5d2
Jennifer huang <133@holbertonschool.com>
2017-02-14 17:39:26 +0000

    Task0- print holberton

---
+++ holbertonschool-low_level_programming/0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+ * main - entry point
+ *
+ * Description: print HOLBERTON w/ _putchar
+ *
+ * Return: 0
+ */
+int main(void)
+{
+_putchar('H');
+_putchar('O');
+_putchar('L');
+_putchar('B');
+_putchar('E');
+_putchar('R');
+_putchar('T');
+_putchar('O');
+_putchar('N');
+
+ _putchar('\n');
+return (0);
+}
---
+++ holbertonschool-low_level_programming/0x02-functions_nested_loops/holberton.h
@@ -0,0 +1,2 @@
+#include <unistd.h>
+int _putchar(char c);
