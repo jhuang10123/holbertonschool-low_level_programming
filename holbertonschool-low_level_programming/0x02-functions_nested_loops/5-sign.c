3f90a2441c253d92b4ca2431f701ec520267b837
Jennifer huang <133@holbertonschool.com>
2017-02-14 21:18:39 +0000

    task 5 - print signs

---
+++ holbertonschool-low_level_programming/0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+ * print_sign - pring sign of number
+ *
+ * @n:integer
+ *
+ * Return:1 if (+), 0, -1 if (-)
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
