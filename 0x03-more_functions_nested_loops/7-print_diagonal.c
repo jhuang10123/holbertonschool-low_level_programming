e89e03b15e3d54de5ce3720690cd5b2b7a4b532d
Jennifer huang <133@holbertonschool.com>
2017-02-17 07:47:53 +0000

    task 7 added

--- 0x03-more_functions_nested_loops/7-print_diagonal.c
+++ 0x03-more_functions_nested_loops/7-print_diagonal.c
@@ -1,5 +1,28 @@
-#include <stdio.h>
 #include "holberton.h"
 /**
+ * print_diagonal - draws a diagonal line in terminal
  *
+ * @n: print
  *
+ * Return: void
+ */
+void print_diagonal(int n)
+{
+	int a;
+	int b;
+
+	if (n > 0)
+	{
+		for (a = 0; a < n; a++)
+		{
+			for (b = 0; b < a; b++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
