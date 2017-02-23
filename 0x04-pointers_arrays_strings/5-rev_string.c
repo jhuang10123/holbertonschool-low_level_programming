0edf34310d44f3ee91a41c8ef4f37a4238ca5e08
Jennifer huang <133@holbertonschool.com>
2017-02-23 07:25:31 +0000

    corrected task 5. It works!

--- 0x04-pointers_arrays_strings/5-rev_string.c
+++ 0x04-pointers_arrays_strings/5-rev_string.c
@@ -6,6 +6,23 @@
  */
 void rev_string(char *s)
 {
+	int j = 0;
+	int i = 0;
+	char temp;
 
-	_putchar ('\n');
+	while (s[i] != 0)
+	{
+		i++;
+	}
+
+	i--;
+
+	while (j < i)
+	{
+		temp = s[j];
+		s[j] = s[i];
+		s[i] = temp;
+		j++;
+		i--;
+	}
 }
