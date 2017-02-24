c214d8e3730987e0be31f7f9bac31fd0461d8e48
Jennifer huang <133@holbertonschool.com>
2017-02-24 19:29:03 +0000

    corrected task 6

--- 0x05-pointers_arrays_strings/6-cap_string.c
+++ 0x05-pointers_arrays_strings/6-cap_string.c
@@ -8,11 +8,13 @@
  */
 char *cap_string(char *cap)
 {
-	int i;
+	int i = 0;
 	char c;
 
 	if (cap[0] >= 'a' && cap[0] <= 'z')
 	{
+		cap[0] = cap[0] - 32;
+	}
 	while (cap[i] != '\0')
 	{
 		c = cap[i - 1];
@@ -20,12 +22,11 @@ char *cap_string(char *cap)
 		     || c == ',' || c == ';' || c == '.'
 		     || c == '!' || c == '?' || c == '"' ||
 		     c == '(' || c == ')' || c == '{' ||
-		     c == '}') && cap[i] >= 'a' && cap[i] <= 'z')
+		     c == '}') && (cap[i] >= 'a' && cap[i] <= 'z'))
 		{
 			cap[i] = cap[i] - 32;
 		}
-		++i;
-	}
+		i++;
 	}
 	return (cap);
 }
