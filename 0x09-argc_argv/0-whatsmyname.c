8086c72826872ffd775d1d82b8a305103e9ecf8c
Jennifer huang <133@holbertonschool.com>
2017-03-04 04:16:43 +0000

    tasks 0 and 1

---
+++ 0x09-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+# include <stdio.h>
+#include "holberton.h"
+/**
+ * main - print program name
+ *
+ * @argc: argument count
+ * @argv: argument vector
+ */
+int main(int argc, char **argv)
+{
+	printf("%s\n", argv[0]);
+	(void)argc;
+	return (0);
+}
