bbeac7c9b5ef31286b8f8cd2911e0477e967f85a
Jennifer huang <133@holbertonschool.com>
2017-03-04 23:52:16 +0000

    correction task 0

--- 0x09-argc_argv/0-whatsmyname.c
+++ 0x09-argc_argv/0-whatsmyname.c
@@ -5,8 +5,9 @@
  *
  * @argc: argument count
  * @argv: argument vector
+ * Return: 0
  */
-int main(int argc, char *argv)
+int main(int argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
 	(void)argc;
