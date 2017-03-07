bdbf9579d963d909ecfe8799b3cf38b84f477500
Jennifer huang <133@holbertonschool.com>
2017-03-07 22:56:13 +0000

    fixed task 0 NULL)

--- 0x0A-malloc_free/0-create_array.c
+++ 0x0A-malloc_free/0-create_array.c
@@ -11,8 +11,8 @@ char *create_array(unsigned int size, char c)
 	char *p;
 	unsigned int i;
 
-	if (size == 0)
-		return (0);
+	if (size == NULL)
+		return (NULL);
 
 	p = malloc(sizeof(char) * size);
 	for (i = 0; i <= size; i++)
