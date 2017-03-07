95107a93e78eb84d9a16c63bcc7800e0bdd75413
Jennifer huang <133@holbertonschool.com>
2017-03-07 22:53:17 +0000

    task 1

--- 0x0A-malloc_free/1-strdup.c
+++ 0x0A-malloc_free/1-strdup.c
@@ -6,7 +6,38 @@
  * @str: string
  * Return: NULL of str == NULL or pointer to string
  */
+int _strlen(char *s);
 char *_strdup(char *str)
 {
+	int i;
+	int j = _strlen(str);
+	char *p;
 
+	if (str == NULL)
+		return (NULL);
+
+
+	p = malloc(sizeof(*str) * j);
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		p[i] = str[i];
+	}
+
+	return (p);
+}
+
+/**
+ * _strlen - finds string length
+ * @s: string
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+		i++;
+
+	return (i);
 }
