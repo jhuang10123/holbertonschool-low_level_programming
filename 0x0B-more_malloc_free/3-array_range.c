38913f55280aa2b089d59613e6a72caf023b4aa5
Jennifer huang <133@holbertonschool.com>
2017-03-09 22:37:43 +0000

    corrected sizeof for task 2

---
+++ 0x0B-more_malloc_free/3-array_range.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * array_range - creates an array of integers.
+ * Description: contain all the values from min (included)
+ *  to max (included), ordered from min to max
+ * @min: min value
+ * @max: max value
+ * Return: pointer or NULL
+ */
+int *array_range(int min, int max)
+{
+
+}
