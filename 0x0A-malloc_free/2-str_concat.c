aa6a5df183e514d38a3dd58f9ba09f43da514bbb
Jennifer huang <133@holbertonschool.com>
2017-03-09 06:33:57 +0000

    fixed syntac errors task 2

--- 0x0A-malloc_free/2-str_concat.c
+++ 0x0A-malloc_free/2-str_concat.c
@@ -11,19 +11,19 @@ char *str_concat(char *s1, char *s2)
 	char *p;
 	int i, j, len1, len2;
 
-        if (s1 == NULL)
-                s1 = "";
-        if (s2 == NULL)
-                s2 = "";
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 
 	for (len1 = 0; s1[len1]; len1++)
 		;
 	for (len2 = 0; s2[len2]; len2++)
 		;
-	p = malloc(sizeof(char) * (len1 + len2 +1));
+	p = malloc(sizeof(char) * (len1 + len2 + 1));
 
-	if (p = NULL)
-		return(NULL);
+	if (p == NULL)
+		return (NULL);
 
 	for (i = 0; i < len1; i++)
 		p[i] = s1[i];
@@ -31,6 +31,8 @@ char *str_concat(char *s1, char *s2)
 	for (j = 0; j < len2; j++)
 		p[i + j] = s2[j];
 
-	return(p);
+	p[i + j] = '\0';
+
+	return (p);
 
 }
