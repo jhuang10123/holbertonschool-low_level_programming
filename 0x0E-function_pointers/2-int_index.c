e8ab17021525361d2f755ce75b12abb14877a511
Jennifer huang <133@holbertonschool.com>
2017-03-15 21:51:28 +0000

    betty checked first 3 files of task 3

--- 0x0E-function_pointers/2-int_index.c
+++ 0x0E-function_pointers/2-int_index.c
@@ -4,9 +4,21 @@
  * @array: array to be searched
  * @size: size of array
  * @cmp: pointer function to compare values
- * Return: integer ot -1
+ * Return: integer or -1
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
+	int i;
 
+	if (array == NULL || cmp == NULL)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]) != 0)
+		{
+			return (i);
+		}
+	}
+	return (-1);
 }
---
+++ 0x0E-function_pointers/3-get_op_func.c
@@ -0,0 +1,29 @@
+#include "3-calc.h"
+#include <stdio.h>
+/**
+ * get_op_func - selects function selected by user
+ * @s: operator
+ * Return: function pointer
+ */
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i;
+
+	i = 0;
+	while (i < 6)
+	{
+		if (strcmp(ops[i].op, s) == 0)
+			return (ops[i].f);
+		i++;
+	}
+	return ("Error\n");
+	exit(99);
+}
