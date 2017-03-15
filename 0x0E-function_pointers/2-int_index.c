4ed350797ae8abd085c813461ed814d2522ca6c5
Jennifer huang <133@holbertonschool.com>
2017-03-15 20:55:44 +0000

    header file correction

---
+++ 0x0E-function_pointers/2-int_index.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+/**
+ * int_index - searches for an integer
+ * @array: array to be searched
+ * @size: size of array
+ * @cmp: pointer function to compare values
+ * Return: integer ot -1
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+
+}
