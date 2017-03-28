54fc33a95aaeae5e071e14478a17c73361762706
Jennifer huang <133@holbertonschool.com>
2017-03-28 16:38:38 +0000

    gcc task 7

--- 0x12-more_singly_linked_lists/7-get_nodeint.c
+++ 0x12-more_singly_linked_lists/7-get_nodeint.c
@@ -10,6 +10,9 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	listint_t *temp;
 	unsigned int count;
 
+	if (head == NULL)
+		return(0);
+
 	count = 0;
 	while (count != index)
 	{
@@ -20,5 +23,3 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	}
 	return (temp);
 }
-
-/* seg fault.null? */
