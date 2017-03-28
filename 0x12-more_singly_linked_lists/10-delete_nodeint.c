2c47b1f6d3de0e6d50eabe6e50b588b980e78ff2
Jennifer huang <133@holbertonschool.com>
2017-03-28 18:32:31 +0000

    task 10 check

--- 0x12-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x12-more_singly_linked_lists/10-delete_nodeint.c
@@ -18,6 +18,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	{
 		*head = temp->next;
 		free(temp);
+		return (1);
 	}
 
 	for (i = 0; temp != NULL && i < (index - 1); i++)
--- 0x12-more_singly_linked_lists/7-get_nodeint.c
+++ 0x12-more_singly_linked_lists/7-get_nodeint.c
@@ -3,7 +3,7 @@
  * get_nodeint_at_index - returns the nth node of a linked list.
  * @head: head of list
  * @index: index of node, starting at 0
- * Return: index or NULL
+ * Return: node or NULL
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
@@ -11,7 +11,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	unsigned int count;
 
 	if (head == NULL)
-		return(NULL);
+		return (NULL);
 
 	count = 0;
 	while (count != index)
@@ -21,5 +21,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 		temp = temp->next;
 
 	}
+
 	return (temp);
+
 }
