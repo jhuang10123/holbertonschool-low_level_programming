a837e0bfcf1e52e9342acab90eafaa77b0aec31b
Jennifer huang <133@holbertonschool.com>
2017-03-31 00:28:45 +0000

    task 3 correction, task 2 incomplete

--- 0x13-bit_manipulation/2-get_bit.c
+++ 0x13-bit_manipulation/2-get_bit.c
@@ -9,7 +9,12 @@ int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned long int mask;
 
-	mask = 1 << index;
-	n = (n & ~ mask);
+	if (index > (sizeof(unsigned long int) * 8))
+		return (-1);
 
+	mask = 1 << index;
+	n = (n | mask);
+	return (1);
 }
+
+/* need revision */
--- 0x13-bit_manipulation/3-set_bit.c
+++ 0x13-bit_manipulation/3-set_bit.c
@@ -14,7 +14,7 @@ int set_bit(unsigned long int *n, unsigned int index)
 		return (-1);
 
 	mask = 1 << index;
-	*n = (*n | i);
+	*n = (*n | mask);
 	return (1);
 
 }
