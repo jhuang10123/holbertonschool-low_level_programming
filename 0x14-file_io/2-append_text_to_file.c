5834f44faaff98fd75e334e8b8346af1175641af
Jennifer huang <133@holbertonschool.com>
2017-04-05 20:39:11 +0000

    task 2

--- 0x14-file_io/2-append_text_to_file.c
+++ 0x14-file_io/2-append_text_to_file.c
@@ -1,8 +1,17 @@
 #include "holberton.h"
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <unisd.h>
+/**
+ * _strlen - finds length of string
+ * @s: string
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+		i++;
+	return (i);
+}
 /**
  * append_text_to_file - appends text at the end of a file
  * @filename: name of file
@@ -11,7 +20,29 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
+	int fd, fwrite, length;
 
 	if (filename == NULL)
 		return (0);
+
+	fd = open(filename, O_RDWR | O_APPEND);
+
+	if (fd == -1)
+		return (-1);
+
+	if (text_content == NULL)
+	{
+		close(fd);
+		return (1);
+	}
+
+	length = _strlen(text_content);
+	fwrite = write(fd, text_content, length);
+
+	if (fwrite == -1)
+	{
+		close(fd);
+		return (-1);
+	}
+	return (1);
 }
