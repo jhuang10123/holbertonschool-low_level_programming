2967e755a39a4ebf1be48889cb60effe27e37262
Jennifer huang <133@holbertonschool.com>
2017-04-05 00:13:05 +0000

    task 1 correction

--- 0x13-bit_manipulation/1-print_binary.c
+++ 0x13-bit_manipulation/1-print_binary.c
@@ -9,7 +9,7 @@ void print_binary(unsigned long int n)
 	int index, flag;
 	unsigned long int m;
 
-	index  = sizeof(n) * 8 - 1;
+	index  = (sizeof(n) * 8) - 1;
 
 	if (n == 0)
 		_putchar ('0');
@@ -18,18 +18,18 @@ void print_binary(unsigned long int n)
 	{
 		m = n >> index;
 
-	if (m & 1)
-	{
-		_putchar ('1');
-		flag = 1;
-	}
-	if (m & 0)
-	{
-		if (flag == 1)
+		if (m & 1)
 		{
-			_putchar ('0');
+			_putchar ('1');
+			flag = 1;
 		}
-	}
+		else
+		{
+			if (flag == 1)
+			{
+				_putchar ('0');
+			}
+		}
 	}
 
 }
