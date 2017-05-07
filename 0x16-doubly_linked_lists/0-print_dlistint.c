76856e5410ec322a7a4800464abce637f24373bf
Jennifer Huang <jhuang1012bn@yahoo.com>
2017-05-06 18:03:40 -0700

    task 0

---
+++ 0x16-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * print_dlistint - prints all the elements of a dlistint_t list
+ * @h: pointer to head of linked list
+ * Return: number of nodes
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	const dlistint_t *temp;
+	unsigned int i;
+
+	i = 0;
+	temp = h;
+	while (temp != NULL)
+	{
+		printf("%d\n", temp->n);
+		temp = temp->next;
+		i++;
+	}
+	return (i);
+}
