e05f790806255276f168cab595d879d7317304cc
Jennifer Huang <jhuang1012bn@yahoo.com>
2017-05-15 23:59:32 -0700

    task 7 - need correction

--- 0x03-more_functions_nested_loops/7-print_diagonal.c
+++ 0x03-more_functions_nested_loops/7-print_diagonal.c
@@ -1,2 +1,31 @@
 #include "holberton.h"
 #include <stdio.h>
+/**
+ * print_diagonal - a diagonal line on the terminal
+ * @n: number of times to print '\'
+ * Return: none
+ */
+void print_diagonal(int n)
+{
+	int lines, spaces;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		for (lines = 1; lines < n; lines++)
+		{
+			for (spaces = 1; spaces < lines; spaces++)
+			{
+				if (lines == spaces)
+				{
+					_putchar('\\');
+					_putchar('\n');
+				}
+				_putchar(' ');
+			}
+
+			_putchar('\n');
+		}
+	}
+}
