1b8d40ba6567b56f56601a5a987eb989b8934905
Jennifer Huang <jhuang1012bn@yahoo.com>
2017-05-15 22:53:50 -0700

    task 6

--- 0x03-more_functions_nested_loops/5-more_numbers.c
+++ 0x03-more_functions_nested_loops/5-more_numbers.c
@@ -1,2 +1,15 @@
 #include "holberton.h"
 #include <stdio.h>
+/**
+ * more_numbers - print 0-14 with new line 10 times
+ * Return: none
+ */
+void more_numbers(void)
+{
+	char i, j;
+
+	for (i = 0; i < 11; i++)
+	{
+
+	}
+}
--- 0x03-more_functions_nested_loops/6-print_line.c
+++ 0x03-more_functions_nested_loops/6-print_line.c
@@ -1,2 +1,15 @@
 #include "holberton.h"
 #include <stdio.h>
+/**
+ * print_line - draws a straight line in the terminal
+ * @n:number of times to print '_'
+ * return: none
+ */
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+		_putchar('_');
+	_putchar('\n');
+}
