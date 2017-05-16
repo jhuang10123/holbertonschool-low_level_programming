15842bb8f9b6c11582340330e73b700e4cf72a75
Jennifer Huang <jhuang1012bn@yahoo.com>
2017-05-16 00:34:58 -0700

    task 8 fizzbuzz

--- 0x03-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x03-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,2 +1,23 @@
 #include "holberton.h"
 #include <stdio.h>
+/*
+ * main - print 1-100, fizz of divisable by 3, buzz if divisable 5
+ * Return: always 0
+ */
+int main()
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0)
+			printf("Fizz");
+		if (i % 5 == 0)
+			printf("Buzz");
+		if ( i % 3 != 0 && i % 5 != 0)
+			printf("%d ", i);
+		if (i < 100)
+			printf(" ");
+	}
+	return (0);
+}
