8db09bd45cbd7af059acc009146d482c238ae01c
Jennifer Huang <jhuang1012bn@yahoo.com>
2017-05-15 22:21:46 -0700

    task 4

--- 0x03-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x03-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,2 +1,17 @@
 #include "holberton.h"
 #include <stdio.h>
+/**
+ * print_most_numbers - print numbers 0-9 except 2, 4
+ * Return: none
+ */
+void print_most_numbers(void)
+{
+	char c;
+
+	for (c = '0'; c < '9'; c++)
+	{
+		if (c != '2' && c != '4')
+			_putchar(c);
+	}
+	_putchar('\n');
+}
