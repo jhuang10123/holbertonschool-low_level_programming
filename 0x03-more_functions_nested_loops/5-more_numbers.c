c6c4c2c10473d8fb7eb84b259e3f9f0e2e32a4f5
Jennifer Huang <jhuang1012bn@yahoo.com>
2017-05-16 09:24:49 -0700

    task 5 removed extra line

--- 0x03-more_functions_nested_loops/5-more_numbers.c
+++ 0x03-more_functions_nested_loops/5-more_numbers.c
@@ -8,11 +8,11 @@ void more_numbers(void)
 {
 	char i, j;
 
-	for (i = 0; i < 11; i++)
+	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j < 15; j++)
 		{
-			if( j >= 10)
+			if (j >= 10)
 			{
 				_putchar(j / 10 + '0');
 			}
