c229e8b670929096389ad2bceeb67517527a2651
Jennifer Huang <jhuang1012bn@yahoo.com>
2017-05-15 22:00:54 -0700

    task 2 mul

--- 0x03-more_functions_nested_loops/2-mul.c
+++ 0x03-more_functions_nested_loops/2-mul.c
@@ -1,2 +1,13 @@
 #include "holberton.h"
 #include <stdio.h>
+/**
+ * mul - multiplies 2 numbers
+ * @a: first number
+ * @b: second number to multiply
+ * Return: result
+ */
+int mul(int a, int b)
+{
+	int result = a * b;
+	return (result);
+}
