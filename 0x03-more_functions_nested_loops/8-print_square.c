81ba98fe9562bb113847e339e4078ddc99067bef
Jennifer Huang <jhuang1012bn@yahoo.com>
2017-05-16 00:14:02 -0700

    task 8

--- 0x03-more_functions_nested_loops/8-print_square.c
+++ 0x03-more_functions_nested_loops/8-print_square.c
@@ -1,2 +1,23 @@
 #include "holberton.h"
 #include <stdio.h>
+/**
+ * print_square - prints a square wiel '#', followed by a new line.
+ * @size: size of square
+ * Return: none
+ */
+void print_square(int size)
+{
+	int rows, i;
+
+	if (size <= 0)
+		_putchar('\n');
+	else
+		for (rows = 1; rows <= size; rows++)
+		{
+			for (i = 0; i < size; i++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+}
