1938337431f0f596c214feb3425c38222a2c8c72
Jennifer Huang <jhuang1012bn@yahoo.com>
2017-05-16 10:05:17 -0700

    task 10

--- 0x03-more_functions_nested_loops/10-print_triangle.c
+++ 0x03-more_functions_nested_loops/10-print_triangle.c
@@ -7,5 +7,26 @@
  */
 void print_triangle(int size)
 {
+	int i, j, k;
 
+	if (size <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+
+	for (i = 0; i <= size; i++) /* rows*/
+	{
+
+		for (j = size; j >= i; j--) /* spaces */
+		{
+			_putchar(' ');
+		}
+		for (k = 1; k <= i; k++)
+		{
+			_putchar('#');
+		}
+		if (i != size) /* new line except last line */
+			_putchar('\n');
+	}
 }
