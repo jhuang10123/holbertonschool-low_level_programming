d83408e72d4e76895ac85de7427bb0d926cdeee1
Jennifer Huang <jhuang1012bn@yahoo.com>
2017-05-15 22:18:56 -0700

    task 3

--- 0x03-more_functions_nested_loops/3-print_numbers.c
+++ 0x03-more_functions_nested_loops/3-print_numbers.c
@@ -1,2 +1,15 @@
 #include "holberton.h"
 #include <stdio.h>
+/**
+ * print_numbers - prints numbers from 0 - 9
+ * Return: none
+ */
+void print_numbers(void)
+{
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+		_putchar(c);
+
+	_putchar('\n');
+}
