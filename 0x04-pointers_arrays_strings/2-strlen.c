04393de0d98e249533c01ec2a0dd5718c77c3fde
Jennifer Huang <jhuang1012bn@yahoo.com>
2017-05-18 01:25:05 -0700

    task 2

---
+++ 0x04-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * _strlen = finds length of a string
+ * @s: string
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; i < s[i]; i++)
+		;
+
+	return (i);
+}
