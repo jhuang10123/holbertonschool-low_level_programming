96456fc119d363180e4ab65987cc8d4f31c9b4de
Jennifer Huang <jhuang1012bn@yahoo.com>
2017-05-18 05:55:18 -0700

    task 9

--- 0x04-pointers_arrays_strings/9-strcpy.c
+++ 0x04-pointers_arrays_strings/9-strcpy.c
@@ -1,4 +1,17 @@
 #include "holberton.h"
 /**
- *
+ * _strcpy - copies a string
+ * @dest: copied string
+ * @src: source string
  */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for(i = 0; src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	return(dest);
+}
