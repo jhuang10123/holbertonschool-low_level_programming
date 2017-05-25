7d45c4ec9337b635d1c71e8744ebc842a1118e62
Jennifer Huang <jhuang1012bn@yahoo.com>
2017-05-25 09:25:30 -0700

    task 5

--- 0x05-pointers_arrays_strings/5-string_toupper.c
+++ 0x05-pointers_arrays_strings/5-string_toupper.c
@@ -1,4 +1,16 @@
 #include "holberton.h"
 /**
- *
+ * string_toupper - changes all lowercase letters of a string to uppercase.
+ * Return: uppercase
  */
+char *string_toupper(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+			str[i] = str[i] - 32;
+	}
+	return (str);
+}
