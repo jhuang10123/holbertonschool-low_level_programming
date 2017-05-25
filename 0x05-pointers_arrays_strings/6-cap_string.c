e4fb69997276b5dc1bccde34837c101786f712f2
Jennifer Huang <jhuang1012bn@yahoo.com>
2017-05-25 10:05:28 -0700

    task 6

--- 0x05-pointers_arrays_strings/6-cap_string.c
+++ 0x05-pointers_arrays_strings/6-cap_string.c
@@ -1,4 +1,24 @@
 #include "holberton.h"
 /**
- *
+ * cap_string - capitalizes all words of a string
+ * @str: string to change
+ * Return: updated string
  */
+char *cap_string(char *str)
+{
+	int i, j;
+
+	char sep[] = " \t\n,;.!?\"(){}";
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		j = 0;
+		while (sep[j] != '\0')
+		{
+			if (str[i] == sep[j] && str[i + 1] >= 'a' && str[i + 1] <= 'z')
+				str[i + 1] = str[i + 1] - 32;
+			j++;
+		}
+	}
+	return (str);
+}
