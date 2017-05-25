b90ecdecb305da68df60aa9ca99a32910db72ad9
Jennifer Huang <jhuang1012bn@yahoo.com>
2017-05-25 09:07:59 -0700

    task 4

--- 0x05-pointers_arrays_strings/4-rev_array.c
+++ 0x05-pointers_arrays_strings/4-rev_array.c
@@ -1,4 +1,22 @@
 #include "holberton.h"
 /**
- *
+ * reverse_array - reverses the content of an array of integers
+ * @a: pointer to array to be reversed
+ * @n:number of elements of the array
  */
+void reverse_array(int *a, int n)
+{
+	int i, j, temp;
+
+	i = 0;
+	j = n - 1; /* points to last element of a */
+
+	while (i < j)
+	{
+		temp = a[i];
+		a[i] = a[j];
+		a[j] = temp;
+		i++;
+		j--;
+	}
+}
