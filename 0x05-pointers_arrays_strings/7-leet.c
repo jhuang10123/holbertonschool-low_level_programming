059cfb37e7351660cb757494c9e297dfb170b6bf
Jennifer Huang <jhuang1012bn@yahoo.com>
2017-05-25 10:36:27 -0700

    task 7

--- 0x05-pointers_arrays_strings/7-leet.c
+++ 0x05-pointers_arrays_strings/7-leet.c
@@ -1,4 +1,24 @@
 #include "holberton.h"
 /**
- *
+ * leet - encodes a string into 1337
+ * @str: string to be encode
+ * Return: encoded string
  */
+char *leet(char *str)
+{
+	int i, j;
+	char letters[] = "AaEeOoTtLl";
+	char num[] = "4433007711";
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		j = 0;
+		while (letters[j] != '\0')
+		{
+			if (str[i] == letters[j])
+				str[i] = num[j];
+			j++;
+		}
+	}
+	return (str);
+}
