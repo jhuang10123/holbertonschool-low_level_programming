aedc5986d0f957a8d7dc7c5a14a22558543e1ab9
Jennifer Huang <jhuang1012bn@yahoo.com>
2017-05-25 10:57:50 -0700

    task 8

--- 0x05-pointers_arrays_strings/8-rot13.c
+++ 0x05-pointers_arrays_strings/8-rot13.c
@@ -1,4 +1,21 @@
 #include "holberton.h"
 /**
- *
+ * rot13 - encodes a string using rot13
+ * @str: str to encode
+ * Return: encoded str
  */
+char *rot13(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if ((str[i] >= 'A' && str[i] <= 'M') || (str[i] >= 'a' && str[i] <= 'm'))
+			str[i] += 13;
+
+		else if ((str[i] >= 'N' && str[i] <= 'Z') || (str[i] >= 'n' && str[i] <= 'z'))
+			str[i] -= 13;
+	}
+	return (str);
+
+}
