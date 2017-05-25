84957144a26a74b2ccd56851d8b5a44351b1d1b5
Jennifer Huang <jhuang1012bn@yahoo.com>
2017-05-25 08:00:46 -0700

    task 1

--- 0x05-pointers_arrays_strings/1-strncat.c
+++ 0x05-pointers_arrays_strings/1-strncat.c
@@ -1,4 +1,25 @@
 #include "holberton.h"
 /**
-*
+* _strncat - concatenates two strings
+* @dest: resulting string
+* @src: string to append
+* Return: dest
 */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+
+	i = 0;
+/* iterate to end of dest */
+	while (dest[i] != '\0')
+		i++;
+
+	for (j = 0; src[j] != '\0' && j < n; j++)
+	{
+		dest[i] = src[j];
+		i++;
+	}
+
+	dest[i] += '\0';
+	return (dest);
+}
