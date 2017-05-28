5a22ed0a177eb211665d58f45ef80d8cc3c618be
Jennifer Huang <jhuang1012bn@yahoo.com>
2017-05-27 23:14:47 -0700

    task 1

--- 0x06-pointers_arrays_strings/1-memcpy.c
+++ 0x06-pointers_arrays_strings/1-memcpy.c
@@ -1,4 +1,17 @@
 #include "holberton.h"
 /**
- *
+ * _memcpy - copies memory area
+ * @n: number of bytes
+ * @src: pointer to source
+ * @dest: pointer to destination memory area
+ * Return: dest
  */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		dest[i] = src[i];
+
+	return (dest);
+}
