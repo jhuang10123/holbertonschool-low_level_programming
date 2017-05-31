b54002035896c542f5cc70b377133a72eca1f9b1
Jennifer Huang <jhuang1012bn@yahoo.com>
2017-05-31 14:21:49 -0700

    task 5 test

--- 0x06-pointers_arrays_strings/5-strstr.c
+++ 0x06-pointers_arrays_strings/5-strstr.c
@@ -1,6 +1,6 @@
 #include "holberton.h"
 /**
- * _strstr - finds 1st occurrence of the substring needle in the string haystack
+ * _strstr - finds 1st occurrence of the substring
  * @haystack: string to be searched
  * @needle: substring
  * Return: pointer to the beginning of the located substring, or NULL
@@ -15,10 +15,14 @@ char *_strstr(char *haystack, char *needle)
 		{
 			if (haystack[i + j] == needle[j])
 			{
+	 /* printf("haystack = %s, i = %d\n", haystack, i);*/
+	 /*  printf("%s\n", haystack + i); */
 				return (haystack + i);
 			}
 				else
 					break;
+			if (needle[j] == '\0')
+				return (NULL);
 
 		}
 	}
