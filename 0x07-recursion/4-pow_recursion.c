5b73871fa4280c1d4f7c30f5bb8e4bc53875c854
Jennifer Huang <jhuang1012bn@yahoo.com>
2017-06-01 15:10:56 -0700

    task 4

--- 0x07-recursion/4-pow_recursion.c
+++ 0x07-recursion/4-pow_recursion.c
@@ -1,7 +1,18 @@
 #include "holberton.h"
 /**
-*
+* _pow_recursion - finds value of x raised to the power of y
 * @x: value
 * @y: power
-* Return: 0 or -1
+* Return: value of x raised to y, or -1
 */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	if (y == 0)
+		return (1);
+
+	x = x * (_pow_recursion(x, y - 1));
+	return (x);
+
+}
