39f617e5f932601fc72ee282228420c7d427b6fe
Jennifer Huang <jhuang1012bn@yahoo.com>
2017-06-01 14:37:38 -0700

    task 2

--- 0x07-recursion/2-strlen_recursion.c
+++ 0x07-recursion/2-strlen_recursion.c
@@ -1,5 +1,18 @@
 #include "holberton.h"
 /**
-*
+* _strlen_recursion - finds string length
 * @s: string
+* Return: string length
 */
+int _strlen_recursion(char *s)
+{
+	int count;
+
+	if (*s == '\0')
+		return (0);
+
+	count = 1;
+
+	count += _strlen_recursion(s + 1);
+	return(count);
+}
