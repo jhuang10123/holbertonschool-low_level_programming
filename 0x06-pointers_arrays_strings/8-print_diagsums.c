637d28dc1bc49dfdcd10b51944e9e4162a434329
Jennifer Huang <jhuang1012bn@yahoo.com>
2017-05-31 17:25:40 -0700

    task 8 need revision

--- 0x06-pointers_arrays_strings/8-print_diagsums.c
+++ 0x06-pointers_arrays_strings/8-print_diagsums.c
@@ -6,13 +6,22 @@
  */
 void print_diagsums(int *a, int size)
 {
-	int i, j, left, right;
+	int i, j, k, left_sum, right_sum;
+
+	left_sum = 0;
+	right_sum = 0;
 
 	for (i = 0; i < size; i++)
 	{
+		k = (i * size) + 1;
+		left_sum = *(a + k);
+
 		for (j = 0; j < size; j++)
 		{
-
+			k = (j * size) + size - 1 - j;
+			right_sum = *(a + k);
 		}
+
 	}
+	printf(" %d, %d\n", left_sum, right_sum);
 }
