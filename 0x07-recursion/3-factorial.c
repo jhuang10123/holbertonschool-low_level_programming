44b590f9a84f92d45a07600737f46859536d376a
Jennifer Huang <jhuang1012bn@yahoo.com>
2017-06-01 14:45:17 -0700

    task 3

--- 0x07-recursion/3-factorial.c
+++ 0x07-recursion/3-factorial.c
@@ -1,5 +1,17 @@
 #include "holberton.h"
 /**
-*
+* factorial - finds factorial of a given number
 * @n: given number
+* Return: factorial
 */
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+
+	if (n == 0)
+		return (1);
+
+	n = n * factorial(n - 1);
+	return (n);
+}
