c74a93e3b0afbe1947946ee3a075d4e04560bc04
Jennifer Huang <jhuang1012bn@yahoo.com>
2017-06-01 15:23:23 -0700

    task 2 correction

--- 0x07-recursion/2-strlen_recursion.c
+++ 0x07-recursion/2-strlen_recursion.c
@@ -11,8 +11,6 @@ int _strlen_recursion(char *s)
 	if (*s == '\0')
 		return (0);
 
-	count = 1;
-
-	count += _strlen_recursion(s + 1);
-	return(count);
+	count = _strlen_recursion(s + 1);
+	return(count + 1);
 }
