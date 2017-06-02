982d65745e7f68fe94a0e4d5f0aa383f113a0883
Jennifer Huang <jhuang1012bn@yahoo.com>
2017-06-02 07:32:17 -0700

    task 5

--- 0x07-recursion/5-sqrt_recursion.c
+++ 0x07-recursion/5-sqrt_recursion.c
@@ -1,6 +1,30 @@
 #include "holberton.h"
 /**
-*
+ * _root - check for squareroot
+ * @n: number to check
+ * @i: multiple
+ * Return: squareroot or -1
+ */
+int _root(int n, int i)
+{
+	if (i * i == n)
+		return (i);
+
+	if (i >= n)
+		return (-1);
+
+	return (_root(n, i + 1));
+}
+
+/**
+* _sqrt_recursion - finds natural square root of a number
 * @n: given number
-* Return: 0 or -1
+* Return:squareroot of n, or -1
 */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+
+	return (_root(n, 1));
+}
