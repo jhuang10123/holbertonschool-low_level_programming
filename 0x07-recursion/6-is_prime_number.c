28b8f34b435b0fd0d43bf1a5d3e0370518385c32
Jennifer Huang <jhuang1012bn@yahoo.com>
2017-06-02 09:50:46 -0700

    task 7

--- 0x07-recursion/6-is_prime_number.c
+++ 0x07-recursion/6-is_prime_number.c
@@ -1,6 +1,52 @@
 #include "holberton.h"
 /**
-*
+ * _prime - check if given number is divisible by i
+ *
+ *
+ * Return:
+ */
+int _prime(int n, int i)
+{
+	int count, res;
+
+	res = 0;
+        if (i == 1)
+		res += _prime (n, i + 1);
+
+/* exit condition */
+	if (i >= n)
+		return (0);
+
+	count = n % i;
+
+	return (count);
+
+/*
+
+
+	_prime(n, i + 1);
+
+	if (n % i != 0)
+		return (1);
+
+	return (0);
+*/
+
+}
+/**
+* is_prime_number - check if given number is prime
 * @n: given number
 * Return: 1 if prime or 0
 */
+int is_prime_number(int n)
+{
+	int count;
+
+	if (n <= 1)
+		return (0);
+
+	return ( _prime(n, 1));
+
+
+	return (count);
+}
--- 0x07-recursion/7-is_palindrome.c
+++ 0x07-recursion/7-is_palindrome.c
@@ -1,5 +1,48 @@
 #include "holberton.h"
 /**
-*
+ * _strlen - finds length of string
+ * @s: string
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+		;
+
+	return (i);
+}
+
+/**
+ * _check_palindrome - check if 2 chars are equal
+ * @s: string to be checked
+ * @i: index for beginning of string
+ * @j: index for end of string
+ * Return: 1 or 0
+ */
+int _check_palindrome(char *s, int i, int j)
+{
+
+	if (i >= j)
+		return (1);
+
+	if (s[i] != s[j])
+		return (0);
+
+	else
+		return(_check_palindrome(s, i + 1, j - 1));
+}
+
+/**
+* is_palindrome - check if string is palindrome
 * @s: string
+* Return: 1 if palindrome, else 0
 */
+int is_palindrome(char *s)
+{
+	int len;
+	len = _strlen(s) - 1;
+
+	return (_check_palindrome(s, 0, len));
+}
