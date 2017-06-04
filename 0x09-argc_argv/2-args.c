685025d908305bb97354cb56a540d322853aa015
Jennifer Huang <jhuang1012bn@yahoo.com>
2017-06-04 16:37:15 -0700

    task 2

--- 0x09-argc_argv/2-args.c
+++ 0x09-argc_argv/2-args.c
@@ -1,12 +1,18 @@
 #include "holberton.h"
 /**
- * main - main
+ * main - prints all arguments
  * @argc: argument count
  * @argc: pointer to argument strings
  * Return: always 0
  */
 int main(int argc, char *argv[])
 {
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
 
 	return (0);
 }
