4f8da6a0abca517c7cc252a75d2a4bdb9e52aaf3
Jennifer Huang <jhuang1012bn@yahoo.com>
2017-06-04 16:58:28 -0700

    task 3 added error

--- 0x09-argc_argv/3-mul.c
+++ 0x09-argc_argv/3-mul.c
@@ -3,13 +3,18 @@
  * main - multiplies two numbers
  * @argc: argument count
  * @argv: pointer to argument strings
- * Return: always 0
+ * Return: 0 on success, 1 on failure
  */
 int main(int argc, char *argv[])
 {
 	int mul;
 	(void) argc;
 
+	if (argc != 3)
+	{
+		printf("Error/n");
+		return (1);
+	}
 	mul = atoi(argv[1]) * atoi(argv[2]);
 	printf("%d\n", mul);
 	return (0);
