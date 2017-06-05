cd04c65c104351f276f0392e0ce7860956ca9a82
Jennifer Huang <jhuang1012bn@yahoo.com>
2017-06-04 18:23:47 -0700

    task 4- need corrections

--- 0x09-argc_argv/4-add.c
+++ 0x09-argc_argv/4-add.c
@@ -1,12 +1,38 @@
 #include "holberton.h"
 /**
- * main - main
+ * main - adds positive number
  * @argc: argument count
  * @argc: pointer to argument strings
  * Return: always 0
  */
 int main(int argc, char *argv[])
 {
+	int i, row, j, sum, len;
+
+	if (argc == 1)
+		printf("0\n");
+
+	len = strlen(argv[row]);
+	sum = 0;
+
+/* iterate through arguments */
+	for (row = 1; row < argc; row++)
+	{
+/* iterate through each argument string */
+		for (col = 0; col < len; col++)
+			{
+				if (argv[i][j] >= '0' && argv[i][j] <= '9');
+				{
+					sum += atoi(argv[i]);
+					printf("%d\n", sum);
+				}
+
+				else
+				{
+					printf("Error\n");
+					return (1);
+				}
+			}
 
 	return (0);
 }
--- 0x09-argc_argv/holberton.h
+++ 0x09-argc_argv/holberton.h
@@ -2,6 +2,7 @@
 #define HOLBERTON_H
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int _putchar(char c);
 
