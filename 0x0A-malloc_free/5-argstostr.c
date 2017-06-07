c546d631c2191749794e094373d7d41dc9d6cd16
Jennifer Huang <jhuang1012bn@yahoo.com>
2017-06-07 10:22:51 -0700

    task 5 need correction

--- 0x0A-malloc_free/5-argstostr.c
+++ 0x0A-malloc_free/5-argstostr.c
@@ -8,7 +8,42 @@
 char *argstostr(int ac, char **av)
 {
 
+	char *ptr;
+	int len, i, j, k, s;
+
+	len = 0;
 
 	if (ac == 0 || av == NULL)
 		return (NULL);
+
+/* finds length of all arguments, + \n + '\0' */
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+		{
+			len++;
+		}
+/* len ++ for \n after each argument */
+		len++;
+	}
+/* len++ for '\0' */
+	len++;
+
+
+	ptr = malloc(sizeof(char) * (len));
+	if (ptr == NULL)
+		return (NULL);
+
+	for (s = 0; s < ac; s++)
+	{
+		for (j = 0, k = 0; av[s][j] != '\0'; j++, k++)
+		{
+			ptr[k] = av[s][j];
+		}
+		ptr[j] = '\n';
+		k++;
+	}
+	ptr[k] = '\0';
+
+	return (ptr);
 }
