6de20786db8410951646f0a70a05e32183c19af3
Jennifer Huang <jhuang1012bn@yahoo.com>
2017-06-06 21:46:22 -0700

    task 5 setup

--- 0x0A-malloc_free/5-argstostr.c
+++ 0x0A-malloc_free/5-argstostr.c
@@ -1,8 +1,12 @@
 #include "holberton.h"
 /**
- *
+ * argstostr - concatenates all the arguments of a program
+ * @ac: argument count
+ * @av: argument vector
+ * Return: Concatenated string of all arguments
  */
-
+char *argstostr(int ac, char **av)
 {
+
 
 }
