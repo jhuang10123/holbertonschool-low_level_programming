5e6aef6eb54d469044e9e58469a140d785efc391
Jennifer Huang <jhuang1012bn@yahoo.com>
2017-06-06 18:10:22 -0700

    task 4 edge cases

--- 0x0A-malloc_free/4-free_grid.c
+++ 0x0A-malloc_free/4-free_grid.c
@@ -8,11 +8,14 @@ void free_grid(int **grid, int height)
 {
 	int i;
 
+	if (grid == NULL || height == 0)
+		return;
+
 	i = 0;
 	while (i < height)
 	{
 		free(grid[i]);
 		i++;
 	}
-	free(*grid);
+	free(grid);
 }
