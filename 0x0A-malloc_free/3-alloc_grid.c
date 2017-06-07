8157fe4373ef5f2d123b4ce1cff6dcb972b87763
Jennifer Huang <jhuang1012bn@yahoo.com>
2017-06-06 17:21:23 -0700

    task 3 & 4, memory leaks

--- 0x0A-malloc_free/3-alloc_grid.c
+++ 0x0A-malloc_free/3-alloc_grid.c
@@ -24,6 +24,9 @@ int **alloc_grid(int width, int height)
 		array[i] = malloc(sizeof(int) * width);
 		if (array[i] == NULL)
 		{
+			for (j = 0; j < i; j++)
+				free(array[j]);
+
 			free(array);
 			return(NULL);
 		}
--- 0x0A-malloc_free/4-free_grid.c
+++ 0x0A-malloc_free/4-free_grid.c
@@ -1,8 +1,18 @@
 #include "holberton.h"
 /**
- *
+ * free_grid - frees a 2 dimensional grid previously created
+ * @grid: grid to be freed
+ * @height: height of grid
  */
-
+void free_grid(int **grid, int height)
 {
+	int i;
 
+	i = 0;
+	while (i < height)
+	{
+		free(grid[i]);
+		i++;
+	}
+	free(*grid);
 }
