04c8d8148536915b9b0ffe48d5f1655fd957222c
Jennifer Huang <jhuang1012bn@yahoo.com>
2017-06-06 18:29:57 -0700

    task 2

--- 0x0A-malloc_free/2-str_concat.c
+++ 0x0A-malloc_free/2-str_concat.c
@@ -22,28 +22,26 @@ int _strlen(char *s)
  */
 char *str_concat(char *s1, char *s2)
 {
-	unsigned int i, j, k, len;
+	unsigned int i, j, len;
 	char *ptr;
 
-	len = _strlen(s1) + _strlen(s2);
+	len = _strlen(s1) + _strlen(s2) + 1;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
 
-	ptr = malloc(sizeof(char) * (len + 1));
+	ptr = malloc(sizeof(char) * len);
 	if (ptr == NULL)
-	{
-		free(ptr);
 		return (NULL);
-	}
+
 	for (i = 0; s1[i] != '\0'; i++)
 		ptr[i] = s1[i];
 
 /* ptr[i] at end of s1, start here s1 left off at ptr[i] */
 	for (j = 0; s2[j] != '\0';i++, j++)
-		ptr[j] = s2[j];
+		ptr[i] = s2[j];
 
 	return (ptr);
 }
