59d28076548be51706c2583b27be50376016aea8
Jennifer Huang <jhuang1012bn@yahoo.com>
2017-06-07 20:55:59 -0700

    task 2

--- 0x0B-more_malloc_free/2-calloc.c
+++ 0x0B-more_malloc_free/2-calloc.c
@@ -1,8 +1,20 @@
 #include "holberton.h"
 /**
- *
+ * _calloc - allocates memory for an array, using malloc
+ * @nmemb: number of elements in array
+ * @size: number of bytes in each element
+ * Return: pointer to address of allocated memory
  */
-
+void *_calloc(unsigned int nmemb, unsigned int size)
 {
+	void *ptr;
 
+	if (nmemb == 0)
+		return (NULL);
+
+	ptr = malloc(sizeof(size) * nmemb);
+	if (ptr == NULL)
+		return (NULL);
+
+	return (ptr);
 }
