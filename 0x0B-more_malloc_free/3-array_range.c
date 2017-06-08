98264d44335ff8962a1956957d487cc52d8867c1
Jennifer Huang <jhuang1012bn@yahoo.com>
2017-06-07 21:38:03 -0700

    added counter, removed abs_val method

--- 0x0B-more_malloc_free/3-array_range.c
+++ 0x0B-more_malloc_free/3-array_range.c
@@ -8,28 +8,19 @@
 int *array_range(int min, int max)
 {
 	int *ptr;
-	int size, abs_min, abs_max, i, k;
+	int size, j, i, k;
 
 	if (min > max)
 		return (NULL);
 
-	if (min < 0)
-		abs_min = -min;
-	else
-		abs_min = min;
-
-	if (max < 0)
-		abs_max = -max;
-	else
-		abs_max = max;
-
-	size = abs_min + abs_max;
+	for (j = min; j <= max; j++)
+		size++;
 
 	ptr = malloc(sizeof(int) * size + 1);
 	if (ptr == NULL)
 		return (NULL);
 
-	for (i = 0, k = min; i < size && k < max; i++, k++)
+	for (i = 0, k = min; i < size && k <= max; i++, k++)
 		ptr[i] = k;
 
 	ptr[i] = '\0';
