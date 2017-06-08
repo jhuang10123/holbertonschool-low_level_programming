a1c68b801dedab36193db9e0853d2df4464de448
Jennifer Huang <jhuang1012bn@yahoo.com>
2017-06-07 20:36:27 -0700

    edited with malloc

--- 0x0B-more_malloc_free/1-string_nconcat.c
+++ 0x0B-more_malloc_free/1-string_nconcat.c
@@ -1,5 +1,20 @@
 #include "holberton.h"
 /**
+ * _strlen - finds length of a string
+ * @s: string
+ * Return: string length
+ */
+int _strlen(char *s)
+{
+	unsigned int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+		;
+
+	return (i);
+}
+
+/**
  * string_nconcat - concatenates two strings up to n byte of s2
  * @s1: string 1
  * @s2: string 2
@@ -10,13 +25,17 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *ptr;
 	unsigned int i, j;
+	int len1;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
 
-	ptr = realloc(s1, n);
+	len1 = _strlen(s1);
+/*	len2 = _strlen(s2); */
+
+	ptr = malloc(sizeof(char) * (len1 + n + 1));
 	if (ptr == NULL)
 		return (NULL);
 
