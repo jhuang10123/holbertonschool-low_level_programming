727a7197b45cc8e785d75d85533a63b60aceb7a9
Jennifer Huang <jhuang1012bn@yahoo.com>
2017-06-07 22:20:50 -0700

    set memory to zero, changed pointer type

--- 0x0B-more_malloc_free/2-calloc.c
+++ 0x0B-more_malloc_free/2-calloc.c
@@ -7,14 +7,19 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void *ptr;
+	char *ptr;
 
-	if (nmemb == 0)
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
 		return (NULL);
 
 	ptr = malloc(sizeof(size) * nmemb);
 	if (ptr == NULL)
 		return (NULL);
+
+	for (i= 0; i < (size * nmemb); i++)
+		ptr[i] = 0;
 
 	return (ptr);
 }
