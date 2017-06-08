adeb348cacdfcc02391738b37422e4a3d5c1301a
Jennifer Huang <jhuang1012bn@yahoo.com>
2017-06-07 21:34:27 -0700

    task 3 - need revision

--- 0x0B-more_malloc_free/3-array_range.c
+++ 0x0B-more_malloc_free/3-array_range.c
@@ -1,8 +1,38 @@
 #include "holberton.h"
 /**
- *
+ * array_range - creates an array of integers
+ * @min: minimum value
+ * @max: max value
+ * Return: pointer to address of allocated memory
  */
-
+int *array_range(int min, int max)
 {
+	int *ptr;
+	int size, abs_min, abs_max, i, k;
 
+	if (min > max)
+		return (NULL);
+
+	if (min < 0)
+		abs_min = -min;
+	else
+		abs_min = min;
+
+	if (max < 0)
+		abs_max = -max;
+	else
+		abs_max = max;
+
+	size = abs_min + abs_max;
+
+	ptr = malloc(sizeof(int) * size + 1);
+	if (ptr == NULL)
+		return (NULL);
+
+	for (i = 0, k = min; i < size && k < max; i++, k++)
+		ptr[i] = k;
+
+	ptr[i] = '\0';
+
+	return (ptr);
 }
