a4e9236cb442a8f2212dee7bcdae77f29ac65d7d
Jennifer Huang <jhuang1012bn@yahoo.com>
2017-06-07 22:23:00 -0700

    corrected malloc size

--- 0x0B-more_malloc_free/2-calloc.c
+++ 0x0B-more_malloc_free/2-calloc.c
@@ -14,11 +14,11 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	ptr = malloc(sizeof(size) * nmemb);
+	ptr = malloc(size * nmemb);
 	if (ptr == NULL)
 		return (NULL);
 
-	for (i= 0; i < (size * nmemb); i++)
+	for (i = 0; i < (size * nmemb); i++)
 		ptr[i] = 0;
 
 	return (ptr);
