75ae31dee7d510d45ff53ec67e1b80d9c8536cf0
Jennifer Huang <jhuang1012bn@yahoo.com>
2017-06-07 20:41:53 -0700

    added len2

--- 0x0B-more_malloc_free/1-string_nconcat.c
+++ 0x0B-more_malloc_free/1-string_nconcat.c
@@ -24,8 +24,7 @@ int _strlen(char *s)
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *ptr;
-	unsigned int i, j;
-	int len1;
+	unsigned int i, j, len1, len2;
 
 	if (s1 == NULL)
 		s1 = "";
@@ -33,7 +32,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		s2 = "";
 
 	len1 = _strlen(s1);
-/*	len2 = _strlen(s2); */
+	len2 = _strlen(s2);
+
+	if (len2 < n)
+		len2 = n;
 
 	ptr = malloc(sizeof(char) * (len1 + n + 1));
 	if (ptr == NULL)
@@ -44,8 +46,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		ptr[i] = s1[i];
 
 /* copies s2 to ptr up to n bytes */
-	for (j = 0; j <= n; j++)
+	for (j = 0; j < len2; j++)
 		ptr[i] = s2[j];
+
+	ptr[i] = '\0';
 
 	return (ptr);
 }
