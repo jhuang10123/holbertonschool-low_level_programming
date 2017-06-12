e9db36a384a62f0864e627bef92bc897e3684683
Jennifer Huang <jhuang1012bn@yahoo.com>
2017-06-12 14:36:36 -0700

    task 0

--- 0x0D-structures_typedef/dog.h
+++ 0x0D-structures_typedef/dog.h
@@ -1,5 +1,17 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
+/**
+ * struct dog - new structure
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: name of owner
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog;
 
 int _putchar(char c);
 
