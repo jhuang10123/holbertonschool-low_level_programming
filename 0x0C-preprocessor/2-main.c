d537d4cd4eda18c77dcd6026398542789c677eb4
Jennifer Huang <jhuang1012bn@yahoo.com>
2017-06-12 13:10:21 -0700

    task 3

---
+++ 0x0C-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ * main - prints the name of the file it was compiled from
+ * Return: 0
+ */
+int main(void)
+{
+	printf(__FILE__);
+	printf("\n");
+	return (0);
+}
--- 0x0C-preprocessor/holberton.h
+++ 0x0C-preprocessor/holberton.h
@@ -1,6 +1,6 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
-
+#include <stdio.h>
 int _putchar(char c);
 
 #endif /*HOLBERTON*/
