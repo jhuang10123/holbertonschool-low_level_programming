32ccd1cc77004addac1a5fb06f0fc1525ea70244
Jennifer Huang <jhuang1012bn@yahoo.com>
2017-06-12 15:36:50 -0700

    task 1

---
+++ 0x0D-structures_typedef/1-init_dog.c
@@ -0,0 +1,12 @@
+#include "dog.h"
+/**
+ * init_dog - initialize a variable of type struct dog
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+
+}
