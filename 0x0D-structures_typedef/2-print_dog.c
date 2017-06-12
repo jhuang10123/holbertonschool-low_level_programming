18cc7f28c20ce7e9862cc5c66aa518055ba6def2
Jennifer Huang <jhuang1012bn@yahoo.com>
2017-06-12 15:47:51 -0700

    task 2

--- 0x0D-structures_typedef/2-print_dog.c
+++ 0x0D-structures_typedef/2-print_dog.c
@@ -1,8 +1,23 @@
 #include "dog.h"
 /**
- *
+ * print_dog - prints a struct dog
+ * @d: pointer to struct dog
  */
+void print_dog(struct dog *d)
 {
+	if (d == NULL)
+		return;
 
+	if (d->name == NULL)
+		printf("(nil)\n");
+	else
+		printf("Name: %s\n", d->name);
+
+	printf("Age: %f\n", d->age);
+
+	if (d->owner == NULL)
+		printf("(nil)\n");
+	else
+		printf("Owner: %s\n", d->owner);
 
 }
--- 0x0D-structures_typedef/dog.h
+++ 0x0D-structures_typedef/dog.h
@@ -1,5 +1,7 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
+#include <stdio.h>
+
 /**
  * struct dog - new structure
  * @name: name of dog
@@ -15,8 +17,8 @@ typedef struct dog
 
 int _putchar(char c);
 void init_dog(struct dog *d, char *name, float age, char *owner);
-/*
 void print_dog(struct dog *d);
+/*
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 */
