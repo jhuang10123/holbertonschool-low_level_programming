6cb673556812b2271c3e26450d7ceed14d5cb6be
Jennifer Huang <jhuang1012bn@yahoo.com>
2017-06-14 09:45:53 -0700

    added NULL check

--- 0x0E-function_pointers/2-int_index.c
+++ 0x0E-function_pointers/2-int_index.c
@@ -10,14 +10,14 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (size <= 0)
+	if (array == NULL || cmp == NULL || size <= 0)
 		return (-1);
 
 	for (i = 1; i < size; i++)
 	{
 		cmp(array[i]);
 
-		if ((cmp(array[i]) != 0)
+		if (cmp(array[i]) != 0)
 			return (i);
 	}
 
