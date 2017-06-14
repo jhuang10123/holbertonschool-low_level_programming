7b36110857d4df3ffc292375e80ea0a82b256e59
Jennifer Huang <jhuang1012bn@yahoo.com>
2017-06-14 09:13:14 -0700

    task 1

--- 0x0E-function_pointers/1-array_iterator.c
+++ 0x0E-function_pointers/1-array_iterator.c
@@ -1,8 +1,16 @@
 #include "function_pointers.h"
 /**
- *
+ * array_iterator - executes a fcn given as parameter on each element of array
+ * @array: given array
+ * @size: size of array
+ * @action: function to execute
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	
+	size_t i;
+
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
 }
