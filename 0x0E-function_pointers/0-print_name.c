0649e2cea2131002622c5e126c9c4843e3e77d34
Jennifer Huang <jhuang1012bn@yahoo.com>
2017-06-14 08:59:49 -0700

    task 0

---
+++ 0x0E-function_pointers/0-print_name.c
@@ -0,0 +1,10 @@
+#include "function_pointers.h"
+/**
+ * print_name - prints a name
+ * @name: name to be printed
+ * @f: function that prints name
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
--- 0x0E-function_pointers/1-array_iterator.c
+++ 0x0E-function_pointers/1-array_iterator.c
@@ -4,5 +4,5 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-
+	
 }
