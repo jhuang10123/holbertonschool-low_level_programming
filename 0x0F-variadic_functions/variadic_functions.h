f24177ff58fcae7d606a70030b5ad7fc72ac7171
Jennifer Huang <jhuang1012bn@yahoo.com>
2017-06-17 18:35:04 -0700

    header edit

--- 0x0F-variadic_functions/variadic_functions.h
+++ 0x0F-variadic_functions/variadic_functions.h
@@ -5,14 +5,15 @@
 #include <stdio.h>
 
 /**
+ * struct identifier - match char* type to corresponding function pointer
  * @type: argument type
  * @f: function pointer to argument type
  */
 typedef struct identifiers
 {
 	char *type;
-	void(*f)();
-}id;
+	void (*f)();
+} id;
 
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
