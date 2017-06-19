55ddec09f58f64ca249d89f8ed19c2ae5f2081a8
Jennifer Huang <jhuang1012bn@yahoo.com>
2017-06-19 14:58:03 -0700

    task 2

--- 0x11-singly_linked_lists/2-add_node.c
+++ 0x11-singly_linked_lists/2-add_node.c
@@ -1,9 +1,42 @@
 #include"lists.h"
 /**
- * @:
- * Return: 
+ * _strlen - finds length of a string
+ * @s: string
+ * Return: string length
+ */
+int _strlen(char *s)
+{
+	unsigned int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+		;
+	return (i);
+}
+
+/**
+ * add_node - adds a new node at the beginning of a list_t list
+ * @head: pointer to the pointer head(used for modifying head)
+ * @str: string in new node
+ * Return: address of new node
  */
 list_t *add_node(list_t **head, const char *str)
 {
+	char *new_str;
+	list_t *new_node;
 
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_str = strdup(str);
+
+	new_node->str = new_str;
+	new_node->len = _strlen(new_str);
+
+	if (head != NULL)
+		new_node->next = *head;
+
+	*head = new_node;
+
+	return (new_node);
 }
