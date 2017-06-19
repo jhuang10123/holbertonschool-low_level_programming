0b055178f55058914f154445f3bff21f1ede1d57
Jennifer Huang <jhuang1012bn@yahoo.com>
2017-06-19 16:54:16 -0700

    added extra printf

--- 0x11-singly_linked_lists/100-first.c
+++ 0x11-singly_linked_lists/100-first.c
@@ -2,7 +2,8 @@
 /**
  * before_main: executes before main function
  */
-void before_main (void)
+void before_main(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
