8657d0d317680a6b62f51d147e40fe592e9cc682
Jennifer Huang <jhuang1012bn@yahoo.com>
2017-06-19 14:22:15 -0700

    task 1

--- 0x11-singly_linked_lists/1-list_len.c
+++ 0x11-singly_linked_lists/1-list_len.c
@@ -1,9 +1,15 @@
 #include"lists.h"
 /**
- * @:
- * Return: 
+ * list_len - returns the number of elements in a linked list_t list
+ * @h: pointer to first node of list
+ * Return: number of elements in list
  */
 size_t list_len(const list_t *h)
 {
+	unsigned int i;
 
+	for (i = 0; h != NULL; i++)
+		h = h->next;
+
+	return (i);
 }
