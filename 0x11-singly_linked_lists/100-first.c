2470ca01a7c9cefbd2c48071c2da04f8b1ef84ff
Jennifer Huang <jhuang1012bn@yahoo.com>
2017-06-19 16:49:04 -0700

    task 5

---
+++ 0x11-singly_linked_lists/100-first.c
@@ -0,0 +1,8 @@
+#include "lists.h"
+/**
+ * before_main: executes before main function
+ */
+void before_main (void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
