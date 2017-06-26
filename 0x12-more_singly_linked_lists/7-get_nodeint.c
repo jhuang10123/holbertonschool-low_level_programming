3366b88cded6cbe388ad182f874ad7be97a08e93
Jennifer Huang <jhuang1012bn@yahoo.com>
2017-06-26 14:03:42 -0700

    set up

--- 0x12-more_singly_linked_lists/7-get_nodeint.c
+++ 0x12-more_singly_linked_lists/7-get_nodeint.c
@@ -2,6 +2,7 @@
 /**
  * get_nodeint_at_index - returns the nth node of a listint_t linked list
  * @head: pointer to first node of list
+ * @idx: index
  * Return: address of the nth node
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
