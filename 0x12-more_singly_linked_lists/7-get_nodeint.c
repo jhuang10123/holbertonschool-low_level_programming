8947139c3c86dca82381e3e8172eac88dbe874c5
Jennifer Huang <jhuang1012bn@yahoo.com>
2017-06-27 16:14:55 -0700

    moved null check

--- 0x12-more_singly_linked_lists/7-get_nodeint.c
+++ 0x12-more_singly_linked_lists/7-get_nodeint.c
@@ -9,14 +9,25 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int i;
 
+/*
 	if (head == NULL)
 		return (NULL);
+
+*/
 
 	for (i = 0; i < index && head != NULL; i++)
 	{
 		head = head->next;
+
+/*
 		if (head->next == NULL)
 			return (NULL);
+*/
 	}
+
+        if (head == NULL)
+                return (NULL);
+
 	return (head);
+
 }
