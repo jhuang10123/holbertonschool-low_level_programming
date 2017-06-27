4a7da77f1d4ee7bc23c35ee8d48ca99b2b886f6d
Jennifer Huang <jhuang1012bn@yahoo.com>
2017-06-27 07:55:18 -0700

    seg fault

--- 0x12-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x12-more_singly_linked_lists/10-delete_nodeint.c
@@ -7,5 +7,39 @@
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
+	listint_t *temp, *next_node;
+	unsigned int i;
 
+	if (*head == NULL)
+		return (-1);
+
+	temp = *head;
+
+	if (index == 0)
+	{
+		*head = (*head)->next;
+		free(temp);
+		return (1);
+	}
+
+	while (temp->next != NULL)
+	{
+		for (i = 0; i < (index - 1); i++)
+		{
+			temp = temp->next;
+		}
+		if (temp->next == NULL)
+		{
+			free(temp);
+			return (1);
+		}
+		else
+		{
+			next_node = temp->next->next;
+			temp = temp->next;
+			free(temp->next);
+			temp->next = next_node;
+		}
+	}
+	return (1);
 }
