3c08adf67ea4006e71065c4a1d1ef84b5709580a
Jennifer Huang <jhuang1012bn@yahoo.com>
2017-06-27 08:08:08 -0700

    checked for head == NULL

--- 0x12-more_singly_linked_lists/7-get_nodeint.c
+++ 0x12-more_singly_linked_lists/7-get_nodeint.c
@@ -9,6 +9,9 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int i;
 
+	if (head == NULL)
+		return (NULL);
+
 	for (i = 0; i < index; i++)
 	{
 		head = head->next;
