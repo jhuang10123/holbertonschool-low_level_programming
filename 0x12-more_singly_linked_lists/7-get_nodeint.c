fb2fd03d3dc46a82fbeb2d93d5c8d1497cad3ece
Jennifer Huang <jhuang1012bn@yahoo.com>
2017-06-27 16:04:14 -0700

    fixed index

--- 0x12-more_singly_linked_lists/7-get_nodeint.c
+++ 0x12-more_singly_linked_lists/7-get_nodeint.c
@@ -12,7 +12,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	if (head == NULL)
 		return (NULL);
 
-	for (i = 0; i < index; i++)
+	for (i = 0; i < index && head != NULL; i++)
 	{
 		head = head->next;
 		if (head->next == NULL)
