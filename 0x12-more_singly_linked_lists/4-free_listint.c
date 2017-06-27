78f41de8a6631c78dcafb6f3e489ddb684386a0c
Jennifer Huang <jhuang1012bn@yahoo.com>
2017-06-27 16:02:49 -0700

    syntax

--- 0x12-more_singly_linked_lists/4-free_listint.c
+++ 0x12-more_singly_linked_lists/4-free_listint.c
@@ -10,7 +10,7 @@ void free_listint(listint_t *head)
 	while (head != NULL)
 	{
 		temp = head;
-		head = head->next;
+		head = temp->next;
 		free(temp);
 	}
 }
--- 0x12-more_singly_linked_lists/5-free_listint2.c
+++ 0x12-more_singly_linked_lists/5-free_listint2.c
@@ -7,10 +7,11 @@ void free_listint2(listint_t **head)
 {
 	listint_t *temp;
 
+
 	while (*head != NULL)
 	{
 		temp = *head;
-		*head = (*head)->next;
+		*head = temp->next;
 		free(temp);
 	}
 	*head = NULL;
