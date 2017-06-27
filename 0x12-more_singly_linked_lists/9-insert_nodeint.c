acdca0b68ea0ca115a7be6c036d36675db625076
Jennifer Huang <jhuang1012bn@yahoo.com>
2017-06-27 16:24:05 -0700

    index update

--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -32,7 +32,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 			return (NULL);
 		}
 
-		for (i = 0; i < (idx - 1); i++)
+		for (i = 0; i < (idx - 1) && temp != NULL; i++)
 			temp = temp->next;
 
 		if (temp->next == NULL)
