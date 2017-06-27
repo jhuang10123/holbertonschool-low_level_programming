593d51d65e87e22783abe314517d052170ff6bac
Jennifer Huang <jhuang1012bn@yahoo.com>
2017-06-27 07:16:19 -0700

    clean up

--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -17,9 +17,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	new_node->n = n;
 
-	if (*head == NULL)
-		return (NULL);
-
 	if (idx == 0)
 	{
 		new_node->next = *head;
@@ -28,20 +25,22 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	else
 	{
 		temp = *head;
-		for (i = 0; i < (idx - 1) && temp!= NULL; i++)
+
+		if (temp == NULL)
+		{
+			free(new_node);
+			return (NULL);
+		}
+
+		for (i = 0; i < (idx - 1); i++)
 			temp = temp->next;
 
 		if (temp->next == NULL)
 			new_node->next = NULL;
 
-		else if (temp == NULL)
-		{
-                        free (new_node);
-                        return (NULL);
-		}
-
 		else
 			new_node->next = temp->next;
+
 		temp->next = new_node;
 	}
 	return (new_node);
