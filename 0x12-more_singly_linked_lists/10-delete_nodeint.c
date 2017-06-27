22aec0ca2f45fe441db00102259d5c23ff855547
Jennifer Huang <jhuang1012bn@yahoo.com>
2017-06-27 08:02:44 -0700

    description update, fixed indentation

--- 0x12-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x12-more_singly_linked_lists/10-delete_nodeint.c
@@ -2,7 +2,7 @@
 /**
  * delete_nodeint_at_index - inserts a new node at a given position
  * @head: pointer to first node of list
- * @idx: index of the list where the new node should be added
+ * @index: index of the list where the new node should be added
  * Return: 1 if it succeeded, -1 if it failed
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
@@ -23,22 +23,22 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	}
 
 	for (i = 0; i < (index - 1) && temp != NULL; i++)
-			temp = temp->next;
+		temp = temp->next;
 
 	if (temp == NULL)
 		return (-1);
 
-		     if (temp->next == NULL)
-		     {
-			     free(temp);
-			     return (1);
-		     }
-		     else
-		     {
-			     next_node = temp->next->next;
-			     free(temp->next);
-			     temp->next = next_node;
-		     }
+	if (temp->next == NULL)
+	{
+		free(temp);
+		return (1);
+	}
+	else
+	{
+		next_node = temp->next->next;
+		free(temp->next);
+		temp->next = next_node;
+	}
 
 	return (1);
 }
