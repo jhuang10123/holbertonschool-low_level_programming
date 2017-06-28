fa2ee7a58b4058fca357a339d789889d3489edaa
Jennifer Huang <jhuang1012bn@yahoo.com>
2017-06-28 08:03:15 -0700

    test files

--- 0x13-bit_manipulation/0-binary_to_uint.c
+++ 0x13-bit_manipulation/0-binary_to_uint.c
@@ -6,5 +6,25 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
+	unsigned int pow, i;
+	int sum;
 
+	pow = 0;
+
+	if (b == NULL)
+		return (0);
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] != 0 && str[i] != 1)
+			return (0);
+	}
+
+	for (i = i; i >= 0; i--)
+	{
+		if (str[i] == 1)
+			sum += str[i] * pow;
+		pow *= 2;
+	}
+	return (sum);
 }
