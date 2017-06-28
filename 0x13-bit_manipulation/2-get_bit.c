792dee77be4b4c28458f9b1e04857c44b7c1d180
Jennifer Huang <jhuang1012bn@yahoo.com>
2017-06-28 10:51:23 -0700

    removed commented out code

--- 0x13-bit_manipulation/2-get_bit.c
+++ 0x13-bit_manipulation/2-get_bit.c
@@ -14,11 +14,6 @@ int get_bit(unsigned long int n, unsigned int index)
 	if (index > size)
 		return (-1);
 
-	/*
-	if (n == 0)
-		_putchar('0');
-	*/
-
 	if ((n >> index) & 1)
 		return (1);
 	else
