745dbe980b969802efd6ffb69a320b43ba772f00
Jennifer Huang <jhuang1012bn@yahoo.com>
2017-06-28 09:30:14 -0700

    using bit shift

--- 0x13-bit_manipulation/0-binary_to_uint.c
+++ 0x13-bit_manipulation/0-binary_to_uint.c
@@ -1,29 +1,52 @@
 #include"holberton.h"
 /**
+ *
+ */
+int _pow(int n)
+{
+	int i = 1;
+	while (n >= 0)
+	{
+		i *= 2;
+	}
+	return (i);
+}
+
+/**
  * binary_to_uint - converts a binary number to an unsigned int
  * @b: pointer to binary string
  * Return: converted number or 0 on failure
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int sum, pow;
+	unsigned int sum;
 	int i;
 
-	pow = 1;
+	sum = 0;
 
 	if (b == NULL)
 		return (0);
 
+/*
 	for (i = 0; b[i] != '\0'; i++)
 	{
 		if (b[i] != '0' && b[i] != '1')
 			return (0);
 	}
 
-	for (i = i; i >= 0; i--)
+*/
+
+	for (i = 0; b[i] == '0' || b[i] == '1'; i++)
 	{
+		sum = sum << 1;
+		if (b[i] == '1')
+			sum++;
+/*
+
 		sum += (b[i] - '0') * pow;
 		pow *= 2;
+*/
 	}
+
 	return (sum);
 }
