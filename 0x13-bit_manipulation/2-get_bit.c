0356cf6e382093ee0f70fc9824c6a33338bf2437
Jennifer Huang <jhuang1012bn@yahoo.com>
2017-06-28 10:50:18 -0700

    task 2

--- 0x13-bit_manipulation/2-get_bit.c
+++ 0x13-bit_manipulation/2-get_bit.c
@@ -7,5 +7,21 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
+	unsigned int size;
+
+	size = sizeof(unsigned long int) * 8 - 1;
+
+	if (index > size)
+		return (-1);
+
+	/*
+	if (n == 0)
+		_putchar('0');
+	*/
+
+	if ((n >> index) & 1)
+		return (1);
+	else
+		return (0);
 
 }
---
+++ 0x13-bit_manipulation/new.c
@@ -0,0 +1,7 @@
+#include"holberton.h"
+/**
+ * print_binary - prints the binary representation of a numb\
+er
+* @n: integer to be printed in binary
+*/
+void print_binary(unsigned long int n)
