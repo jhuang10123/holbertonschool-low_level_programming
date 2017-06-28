16803aabf2e2643710200e77439808a25a08febf
Jennifer Huang <jhuang1012bn@yahoo.com>
2017-06-27 17:41:13 -0700

    fixed idx at 0

--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -26,8 +26,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 /* edge case - if at beg. of list */
 	if (idx == 0)
 	{
-		new_node->next = *head;
-		*head = new_node;
+		new_node->next = temp->next;
+		temp->next = new_node;
 		return (new_node);
 	}
 	for (i = 0; i < (idx - 1); i++)
