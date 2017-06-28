5764ecb2a0e3f1804b036801774fad51c7663052
Jennifer Huang <jhuang1012bn@yahoo.com>
2017-06-28 08:19:09 -0700

    output error

--- 0x13-bit_manipulation/0-binary_to_uint.c
+++ 0x13-bit_manipulation/0-binary_to_uint.c
@@ -6,24 +6,23 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int pow, i;
-	int sum;
+	unsigned int sum, pow;
+	int i;
 
-	pow = 0;
+	pow = 1;
 
 	if (b == NULL)
 		return (0);
 
-	for (i = 0; str[i] != '\0'; i++)
+	for (i = 0; b[i] != '\0'; i++)
 	{
-		if (str[i] != 0 && str[i] != 1)
+		if (b[i] != '0' && b[i] != '1')
 			return (0);
 	}
 
 	for (i = i; i >= 0; i--)
 	{
-		if (str[i] == 1)
-			sum += str[i] * pow;
+		sum += (b[i] - '0') * pow;
 		pow *= 2;
 	}
 	return (sum);
--- 0x13-bit_manipulation/holberton.h
+++ 0x13-bit_manipulation/holberton.h
@@ -1,5 +1,6 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
+#include <stdlib.h>
 
 int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
