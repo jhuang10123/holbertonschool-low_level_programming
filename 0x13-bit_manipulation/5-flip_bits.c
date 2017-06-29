ec7c3458a7c93bbe958ed3b391f9bf6f5f4bfa88
Jennifer Huang <jhuang1012bn@yahoo.com>
2017-06-28 20:09:49 -0700

    task 5

--- 0x13-bit_manipulation/5-flip_bits.c
+++ 0x13-bit_manipulation/5-flip_bits.c
@@ -7,5 +7,20 @@
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
+/*
+m ^ n. compare difference
+ */
 
+	unsigned long int diff, num;
+	int i;
+
+	num = 0;
+	diff = n ^ m;
+
+	for (i = sizeof(unsigned long int) * 8 - 1; i >= 0; i--)
+	{
+		if ((diff >> i) & 1)
+			num++;
+	}
+	return (num);
 }
