7c15ab282c2707be65b5bff2981a8290d7c5749c
Jennifer Huang <jhuang1012bn@yahoo.com>
2017-07-03 18:27:03 -0700

    check filename

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -25,13 +25,16 @@ int create_file(const char *filename, char *text_content)
 {
 	int fd, fd_write, len;
 
+	if (filename == NULL)
+		return (-1);
+
 	fd = open(filename, O_RDONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (fd == -1)
 		return (-1);
 
 	len = _strlen(text_content);
 
-	fd_write = write(fd, filename, len);
+	fd_write = write(fd, text_content, len);
 	if (fd_write == -1 || text_content == NULL)
 	{
 		close(fd);
