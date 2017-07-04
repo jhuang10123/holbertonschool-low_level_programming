7ffaef589a628143ad79b90c925215b974ad5808
Jennifer Huang <jhuang1012bn@yahoo.com>
2017-07-03 18:30:40 -0700

    edited return value

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -28,13 +28,13 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_RDONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (fd == -1)
 		return (-1);
 	if (text_content == NULL)
 	{
 		close(fd);
-		return (-1);
+		return (1);
 	}
 
 	len = _strlen(text_content);
