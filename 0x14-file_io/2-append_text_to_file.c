576a722ee276e3799c0a83c5c3e113784fe7c3ec
Jennifer Huang <jhuang1012bn@yahoo.com>
2017-07-03 18:48:07 -0700

    task 2 incorrect output

--- 0x14-file_io/2-append_text_to_file.c
+++ 0x14-file_io/2-append_text_to_file.c
@@ -1,5 +1,20 @@
 #include"holberton.h"
 /**
+ * _strlen - finds length of a string
+ * @s: string
+ * Return: string length
+ */
+int _strlen(char *s)
+{
+	unsigned int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+		;
+
+	return (i);
+}
+
+/**
  * append_text_to_file - appends text at the end of a file
  * @filename: name of file to create
  * @text_content: content in file
@@ -7,5 +22,27 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
+	int fd, fd_write, len;
+
+	len = _strlen(text_content);
+
+	fd = open(filename, O_RDWR | O_APPEND);
+	if (fd == -1)
+		return (-1);
+
+	if (text_content == NULL)
+	{
+		close(fd);
+		return (1);
+	}
+
+	fd_write = write(fd, text_content, len);
+	if(fd_write == -1)
+	{
+		close(fd);
+		return (-1);
+	}
+
+	return (1);
 
 }
