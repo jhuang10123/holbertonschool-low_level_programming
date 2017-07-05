eda3319731605e95c5d7a2b733f532c4f4a885af
Jennifer Huang <jhuang1012bn@yahoo.com>
2017-07-05 04:48:35 -0700

    task 3

--- 0x14-file_io/3-cp.c
+++ 0x14-file_io/3-cp.c
@@ -9,22 +9,19 @@ void exit_fcn(int n, char *file)
 	switch (n)
 	{
 	case 97:
-		dprintf(STDERR_FILENO,"Usage: cp file_from file_to\n");
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	case 98:
-		dprintf(STDERR_FILENO,"Error: Can't read from file %s\n", file);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file);
 			exit(98);
 	case 99:
-		dprintf(STDERR_FILENO,"Error: Can't write to %s\n", file);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file);
 			exit(99);
 	default:
-		dprintf(STDERR_FILENO,"Error: Can't close fd %d\n", n);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", n);
 			exit(100);
-
 	}
-
 }
-
 /**
  * check_close - close file descriptor
  * @fd: file descriptor
@@ -40,20 +37,18 @@ int check_close(int fd, char *file)
 		exit_fcn(100, file);
 	return (n);
 }
-
 /**
  * main - copies content of one file to the other
  * @argc: argument count
  * @argv: argument vector
  * Return: 0
  */
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	int fd_from, fd_to, read_count, write_count, fd_close, fd_close2;
 	char *buffer, *file_from, *file_to;
 
-	file_from = argv[1];
-	file_to = argv[2];
+	file_from = argv[1], file_to = argv[2];
 
 	if (argc != 3)
 		exit_fcn(97, NULL);
@@ -71,12 +66,10 @@ int main (int argc, char *argv[])
 	buffer = malloc(sizeof(char) * BUFLEN);
 	if (buffer == NULL)
 		return (1);
-
 	read_count = read(fd_from, buffer, BUFLEN);
 	if (read_count == -1)
 	{
-		check_close(fd_from, file_from);
-		check_close(fd_to, file_to);
+		check_close(fd_from, file_from), check_close(fd_to, file_to);
 		exit_fcn(98, file_from);
 	}
 	while (read_count > 0)
@@ -96,15 +89,8 @@ int main (int argc, char *argv[])
 			exit_fcn(98, file_from);
 		}
 	}
-
-	fd_close = close(fd_from);
-	if (fd_close == -1)
-		exit_fcn(100, NULL);
-
-	fd_close2 = close(fd_to);
-		if (fd_close2 == -1)
-			exit_fcn(100, NULL);
-
+	fr_close = check_close(fd_from, NULL);
+	to_close = check_close(fd_to, NULL);
 	free(buffer);
-	return(0);
+	return (0);
 }
