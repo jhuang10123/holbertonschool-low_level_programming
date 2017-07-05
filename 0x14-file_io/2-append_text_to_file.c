0223f2ef7146faf108c996025ab174836316ab16
Jennifer Huang <jhuang1012bn@yahoo.com>
2017-07-05 05:11:59 -0700

    edited return value for failure

--- 0x14-file_io/2-append_text_to_file.c
+++ 0x14-file_io/2-append_text_to_file.c
@@ -36,7 +36,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (text_content == NULL)
 	{
 		close(fd);
-		return (1);
+		return (-1);
 	}
 
 	fd_write = write(fd, text_content, len);
