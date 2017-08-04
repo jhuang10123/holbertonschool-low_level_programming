9eb5fa4e5a6377f13d0dd1e0a8c4ba934158d2ce
Jennifer Huang <jhuang1012bn@yahoo.com>
2017-08-04 09:00:09 -0700

    task 5

--- 0x16-doubly_linked_lists/5-get_dnodeint.c
+++ 0x16-doubly_linked_lists/5-get_dnodeint.c
@@ -7,5 +7,20 @@
  */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
+	unsigned int i;
+
+	if (head == NULL)
+		return (NULL);
+
+	for (i = 0; i < index; i++)
+	{
+		if (head->next == NULL)
+			return (NULL);
+
+		head = head->next;
+	}
+
+	return (head);
+
 
 }
