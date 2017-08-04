684e172e35f66a67b0f21fc5904234e9104869e7
Jennifer Huang <jhuang1012bn@yahoo.com>
2017-08-04 09:13:39 -0700

    task 7 need correction

--- 0x16-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x16-doubly_linked_lists/7-insert_dnodeint.c
@@ -8,5 +8,24 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
+	dlistint_t *temp, *new;
+	unsigned int i;
 
+	temp = *h;
+	for (i = 0; i < idx; i++)
+	{
+		if (temp->next == NULL)
+			return (NULL);
+		temp = temp->next;
+	}
+
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->next = temp->next;
+	new->prev = temp->prev;
+	new->n = n;
+
+	return (new);
 }
