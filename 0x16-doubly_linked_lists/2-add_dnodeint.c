498633058d994235953e164d82f4897fae58cbf1
Jennifer Huang <jhuang1012bn@yahoo.com>
2017-08-11 14:10:46 -0700

    revert back

--- 0x16-doubly_linked_lists/2-add_dnodeint.c
+++ 0x16-doubly_linked_lists/2-add_dnodeint.c
@@ -19,9 +19,11 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 
 
 	new->n = n;
+	new->prev = NULL;
 	new->next = *head;
 
+	(*head)->prev = new;
 	*head = new;
 
-	return (*head);
+	return (new);
 }
