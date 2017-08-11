71f8c02b385d84686939214770ad918a90450758
Jennifer Huang <jhuang1012bn@yahoo.com>
2017-08-11 16:10:29 -0700

    rm extra lines

--- 0x16-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x16-doubly_linked_lists/8-delete_dnodeint.c
@@ -11,8 +11,6 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	dlistint_t *temp;
 	unsigned int i;
 
-
-/*list should not be empty */
 	if (head == NULL || *head == NULL)
 		return (-1);
 
