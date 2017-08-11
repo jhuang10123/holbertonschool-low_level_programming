cbead41ef46e13a8b0c2e968d2e9b5cca4e966da
Jennifer Huang <jhuang1012bn@yahoo.com>
2017-08-11 14:15:07 -0700

    check if *head == null

--- 0x16-doubly_linked_lists/2-add_dnodeint.c
+++ 0x16-doubly_linked_lists/2-add_dnodeint.c
@@ -22,7 +22,9 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	new->prev = NULL;
 	new->next = *head;
 
-	(*head)->prev = new;
+	if (*head != NULL)
+		(*head)->prev = new;
+
 	*head = new;
 
 	return (new);
