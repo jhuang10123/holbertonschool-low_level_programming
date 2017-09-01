851c86f7f8e506da230369694d34f193355235b3
Jennifer Huang <jhuang1012bn@yahoo.com>
2017-09-01 15:23:40 -0700

    changed size

--- 0x19-hash_tables/0-hash_table_create.c
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -14,7 +14,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 
 /* allocate and initialize space for nodes */
-	table->array = calloc(size, sizeof(hash_node_t));
+	table->array = calloc(size, sizeof(hash_node_t *));
 	if (table->array == NULL)
 	{
 		free (table);
