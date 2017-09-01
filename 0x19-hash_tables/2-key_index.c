68dbdb1328166c205e53af8d92b1c76916b7ee6f
Jennifer Huang <jhuang1012bn@yahoo.com>
2017-09-01 10:26:20 -0700

    idx

--- 0x19-hash_tables/2-key_index.c
+++ 0x19-hash_tables/2-key_index.c
@@ -1,4 +1,15 @@
 #include "hash_tables.h"
 /**
- *
+ * key_index - finds index of a key
+ * @key: key
+ * @size: size of hash table
+ * Return: index of key
  */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	unsigned long int index;
+
+	index = hash_djb2(key) % size;
+
+	return (index);
+}
