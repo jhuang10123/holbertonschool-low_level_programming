ff4c603b97a342e69c2a07a8ecaad70e602d5d0c
Jennifer Huang <jhuang1012bn@yahoo.com>
2017-10-10 21:22:06 -0700

    10

--- 0x1C-binary_trees/11-binary_tree_size.c
+++ 0x1C-binary_trees/11-binary_tree_size.c
@@ -6,4 +6,12 @@
  */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
+
+        if (tree == NULL)
+                return (0); 
+
+        return (binary_tree_size(tree->left) + binary_tree_size(tree->right) + 1);
 }
+
+/* size: adding all nodes.
+   count left recursivesy + right recursively + 1(for root) */
