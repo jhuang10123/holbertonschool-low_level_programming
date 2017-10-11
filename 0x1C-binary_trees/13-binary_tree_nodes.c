892574dce18c817e90573febd50d38c20018ff80
Jennifer Huang <jhuang1012bn@yahoo.com>
2017-10-10 21:53:25 -0700

    task 13

--- 0x1C-binary_trees/13-binary_tree_nodes.c
+++ 0x1C-binary_trees/13-binary_tree_nodes.c
@@ -6,4 +6,14 @@
  */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
+        size_t count;
+
+        if (tree == NULL || (tree->left == NULL && tree->right == NULL))
+                return (0);
+
+        count = 1;
+        count += binary_tree_nodes(tree->left);
+        count += binary_tree_nodes(tree->right);
+        return (count);
+
 }
