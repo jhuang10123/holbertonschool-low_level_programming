288179186e006798a8ddca894c491b8f903e77f5
wendysegura <118@holbertonschool.com>
2017-10-26 16:28:37 +0000

    fixed betty

--- 0x1C-binary_trees/5-binary_tree_is_root.c
+++ 0x1C-binary_trees/5-binary_tree_is_root.c
@@ -6,8 +6,8 @@
  */
 int binary_tree_is_root(const binary_tree_t *node)
 {
-       if (node->parent == NULL && node != NULL)
-                return (1);
-        else
-                return (0);
+	if (node->parent == NULL && node != NULL)
+		return (1);
+	else
+		return (0);
 }
