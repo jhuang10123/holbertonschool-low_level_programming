c598ee9270d10c7adf4da2fee5b30da3cdda33f2
wendysegura <118@holbertonschool.com>
2017-10-26 17:28:23 +0000

    betty fixed

--- 0x1C-binary_trees/14-binary_tree_balance.c
+++ 0x1C-binary_trees/14-binary_tree_balance.c
@@ -38,7 +38,7 @@ int binary_tree_balance(const binary_tree_t *tree)
 	left = right = 0;
 
 	if (tree == NULL)
- 		return (0);
+		return (0);
 
 	left = binary_tree_height(tree->left);
 	right = binary_tree_height(tree->right);
