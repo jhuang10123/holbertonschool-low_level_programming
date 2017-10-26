307ed015a100862270a42a6b67cb898be298dc1a
wendysegura <118@holbertonschool.com>
2017-10-26 21:12:43 +0000

    added edge and fix betty

--- 0x1C-binary_trees/16-binary_tree_is_perfect.c
+++ 0x1C-binary_trees/16-binary_tree_is_perfect.c
@@ -9,21 +9,23 @@
 size_t binary_tree_height(const binary_tree_t *tree)
 {
 
-    size_t left, right;
+	size_t left, right;
 
 /* account for leaf node */
-    if (tree == NULL || (tree->left == NULL && tree->right == NULL))
-        return (0);
+	if (tree == NULL || (tree->left == NULL && tree->right == NULL))
+		return (0);
+	if (tree->left != NULL && tree->right != NULL)
+		return (0);
 
-    left = right = 0;
+	left = right = 0;
 
-    left = binary_tree_height(tree->left);
-    right = binary_tree_height(tree->right);
+	left = binary_tree_height(tree->left);
+	right = binary_tree_height(tree->right);
 
-    if (left > right)
-        return (1 + left);
-    else
-        return (1 + right);
+	if (left > right)
+		return (1 + left);
+	else
+		return (1 + right);
 
 
 }
@@ -36,24 +38,24 @@ size_t binary_tree_height(const binary_tree_t *tree)
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
 
-    binary_tree_t *left, *right;
+	binary_tree_t *left, *right;
 
-    left = tree->left;
-    right = tree->right;
+	left = tree->left;
+	right = tree->right;
 
-    if (tree == NULL)
-        return (0);
+	if (tree == NULL)
+		return (0);
 
 /* check if have 2 children */
-    if (left == NULL || right == NULL)
-        return (0);
+	if (left == NULL || right == NULL)
+		return (0);
 
-    if (binary_tree_height(left) == binary_tree_height(right))
-    {
-        if (binary_tree_is_perfect(left) == binary_tree_is_perfect(right))
-            return (1);
-    }
+	if (binary_tree_height(left) == binary_tree_height(right))
+	{
+	if (binary_tree_is_perfect(left) == binary_tree_is_perfect(right))
+		return (1);
+	}
 
-    return (0);
+	return (0);
 
 }
