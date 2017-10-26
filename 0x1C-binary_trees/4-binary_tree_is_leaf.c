36793ede5f7eab1801512de270daa082f0937b69
wendysegura <118@holbertonschool.com>
2017-10-26 16:27:00 +0000

    fixed betty

--- 0x1C-binary_trees/4-binary_tree_is_leaf.c
+++ 0x1C-binary_trees/4-binary_tree_is_leaf.c
@@ -6,11 +6,11 @@
  */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-    if (node == NULL)
-        return (0);
+	if (node == NULL)
+		return (0);
 
-    if (node->right == NULL && node->left == NULL)
-        return (1);
-    else
-        return (0);
+	if (node->right == NULL && node->left == NULL)
+		return (1);
+	else
+		return (0);
 }
