4f7bab827a61e6f64609d82c95a67fefd262912d
wendysegura <118@holbertonschool.com>
2017-10-26 17:32:06 +0000

    fixed edgecase

--- 0x1C-binary_trees/2-binary_tree_insert_right.c
+++ 0x1C-binary_trees/2-binary_tree_insert_right.c
@@ -18,7 +18,8 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 		new->parent = parent;
 
 
-	if (parent != NULL)
+	if (parent == NULL)
+		return (NULL);
 	{
 		if (parent->right == NULL)
 		{
