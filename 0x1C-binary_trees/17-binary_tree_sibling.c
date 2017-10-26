6cea0b61a4af43a1fa8c6f4b751104d45fc525cf
wendysegura <118@holbertonschool.com>
2017-10-26 21:15:23 +0000

    fixed betty

--- 0x1C-binary_trees/17-binary_tree_sibling.c
+++ 0x1C-binary_trees/17-binary_tree_sibling.c
@@ -6,11 +6,11 @@
  */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
-    if (node == NULL || node->parent == NULL)
-        return (NULL);
+	if (node == NULL || node->parent == NULL)
+		return (NULL);
 
-        if (node->parent->left == node)
-            return (node->parent->right);
-        else
-            return (node->parent->left);
+	if (node->parent->left == node)
+		return (node->parent->right);
+	else
+		return (node->parent->left);
 }
