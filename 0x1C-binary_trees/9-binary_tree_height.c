36df669f417768a934ec841a329286ae41b01478
wendysegura <118@holbertonschool.com>
2017-10-26 16:57:30 +0000

    fixed betty

--- 0x1C-binary_trees/9-binary_tree_height.c
+++ 0x1C-binary_trees/9-binary_tree_height.c
@@ -7,21 +7,21 @@
 size_t binary_tree_height(const binary_tree_t *tree)
 {
 
-    size_t left, right;
+	size_t left, right;
 
 /* account for leaf node */
-    if (tree == NULL || (tree->left == NULL && tree->right == NULL))
-                return (0);
+	if (tree == NULL || (tree->left == NULL && tree->right == NULL))
+		return (0);
 
-        left = right = 0;
+	left = right = 0;
 
-        left = binary_tree_height(tree->left);
-        right = binary_tree_height(tree->right);
+	left = binary_tree_height(tree->left);
+	right = binary_tree_height(tree->right);
 
-        if (left > right)
-                return (1 + left);
-        else
-                return (1 + right);
+	if (left > right)
+		return (1 + left);
+	else
+		return (1 + right);
 
 
 }
