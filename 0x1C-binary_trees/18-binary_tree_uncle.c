9105c427eef87eb454fc40f09059b881127bbb18
wendysegura <118@holbertonschool.com>
2017-10-26 21:17:08 +0000

    betty fix

--- 0x1C-binary_trees/18-binary_tree_uncle.c
+++ 0x1C-binary_trees/18-binary_tree_uncle.c
@@ -7,11 +7,11 @@
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
 
-    if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
-        return (NULL);
+	if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
+		return (NULL);
 
-    if (node->parent->parent->right == node->parent)
-        return (node->parent->parent->left);
-    else
-        return (node->parent->parent->right);
+	if (node->parent->parent->right == node->parent)
+		return (node->parent->parent->left);
+	else
+		return (node->parent->parent->right);
 }
