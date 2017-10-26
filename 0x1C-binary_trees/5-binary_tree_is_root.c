25ef2995e1b2dbf6bf56159a57b4d87e2b8e410e
wendysegura <118@holbertonschool.com>
2017-10-26 09:43:55 -0700

    changed order

--- 0x1C-binary_trees/5-binary_tree_is_root.c
+++ 0x1C-binary_trees/5-binary_tree_is_root.c
@@ -6,11 +6,12 @@
  */
 int binary_tree_is_root(const binary_tree_t *node)
 {
+
+	if (node == NULL)
+		return (0);
 	if (node->parent == NULL)
 		return (1);
 	else
 		return (0);
 
-	if (node == NULL)
-		return (0);
 }
