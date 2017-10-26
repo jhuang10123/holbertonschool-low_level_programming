941912c01bd706f7260562416c3447996c76ef02
wendysegura <118@holbertonschool.com>
2017-10-26 16:42:00 +0000

    seperated edge case

--- 0x1C-binary_trees/5-binary_tree_is_root.c
+++ 0x1C-binary_trees/5-binary_tree_is_root.c
@@ -6,8 +6,11 @@
  */
 int binary_tree_is_root(const binary_tree_t *node)
 {
-	if (node->parent == NULL && node != NULL)
+	if (node->parent == NULL)
 		return (1);
 	else
+		return (0);
+
+	if (node == NULL)
 		return (0);
 }
