9ecf2297f02954668b3b1f57534ad294e00eadfa
jhuang10123 <133@holbertonschool.com>
2017-12-06 23:13:21 +0000

    task 1

--- 0x1D-search_algorithms/1-binary.c
+++ 0x1D-search_algorithms/1-binary.c
@@ -1,8 +1,52 @@
-#include search_algos.h
+#include "search_algos.h"
 /**
- *
+ * print_array: prints an array of int
+ * @start: starting point
+ * @end: ending point
+ */
+void print_array(int *array, size_t start, size_t end)
+{
+  size_t i;
+
+  printf("Searching in array: ");
+
+  for (i = start; i < end; i++)
+    printf("%d, ", array[i]);
+  printf("%d\n", array[end]);
+}
+/**
+ * binary_search - uses binary search to
+ * search for a value in an array
+ * @array: array to be searched
+ * @size: size of array
+ * @value: value to be searched
+ * Return: index or -1
  */
 int binary_search(int *array, size_t size, int value)
 {
+  size_t mid, left, right;
 
+  if (array == NULL)
+    return (-1);
+
+  left = mid = 0;
+  right = size - 1;
+
+  while (left <= right)
+    {
+      print_array(array, left, right);
+      mid = (right + left) / 2;
+      if (value == array[mid])
+	return (mid);
+      else if (value < array[mid])
+	{
+	  right = mid - 1;
+	}
+      else
+	{
+	  left = mid + 1;
+	}
+
+    }
+  return (-1);
 }
